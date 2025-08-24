208c6ad12d5bb972f710ae596b570c00e7abad97
RaviChaudhari1 <ravichaudhari0205@gmail.com>
2025-08-25 00:25:11 +0530

    add comment, like, playlist, and tweet models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -170,6 +170,7 @@ const logoutUser = asyncHandler(async (req, res) => {
         req.user._id,
         {
             $set: { refreshToken: undefined }
+            // unset: {refreshToken: 1}
         },
         {
             new: true
