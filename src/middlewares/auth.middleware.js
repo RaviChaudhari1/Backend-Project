469ddc634c8c76c692253e9ff37d987565671be5
RaviChaudhari1 <ravichaudhari0205@gmail.com>
2025-08-24 16:05:40 +0530

    logout user

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+import { User } from "../models/User.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    // access tokens from cookies (cookie-parser middleware)
+    try {
+        const token = req.cookies?.accessToken || req.heaader("Authorization")?.replace("Bearer ", "");
+    
+        if (!token) {
+            throw new ApiError(401, "Unauthorized - No token provided");
+        }
+    
+        // verify token
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._Id).select("-password -refreshToken");
+        
+        if (!user) {
+            throw new ApiError(401, "Invalid Access Token");
+        }
+    
+        req.user = user; // attach user to request object
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.messsage || "Unauthorized - Invalid token");
+    }
+
+})
